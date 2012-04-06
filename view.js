a35e8b197e7dff1b9c47a077ae3987144ded5cf2
Morgan Paull <mpaull@fas.harvard.edu>
2012-04-05 20:37:41 -0400

    added facets column to view

--- view.js
+++ view.js
@@ -1,6 +1,8 @@
 function View(m){
     //This object draws stuff in HTML
     //View settings go here (i.e. how many things per row, etc)
+	
+	
 }
 View.prototype.clear_filters = function(){
     //put loading graphic in filters pane
@@ -10,9 +12,42 @@ View.prototype.clear_results = function(){
 }
 View.prototype.draw_filters = function(filters){
     //uses return of m.get_filters()
+    
+	var html = ''
+	var current = filters.current;
+	var suggested = filters.suggested;
+    
+    html+='<ul class="nav nav-tabs nav-stacked">';
+    
+	for (var index in current) {
+    	html += '<li><a href="#">' + current[index][0] + ': ' + current[index][1] + '</a></li>';
+    }
+    	
+    	html+='</ul><ul class="nav nav-list">';
+    	
+    for (var index in suggested) {
+    	html += '<li class="nav-header">' + index + '</li>';
+    	
+    	for(var n in suggested[index]) {
+    	html += '<li><a href="#">' + suggested[index][n] + '</a></li>';
+    	}
+    }
+    html += '</ul>';
+    
+    $('#facets').html(html);
 }
 View.prototype.draw_grid = function(item_list){
     //uses return of m.get_items()
+    
+    //parse array of results
+    var html = "";
+    
+    
+    for (var i = 0; i<item_list.length; i++) {
+    	html += JSON.stringify(item_list[i]) + "<br><br><br>";
+    }
+    
+    $('#results').html(html);
 }
 View.prototype.draw_list = function(item_list){
     //uses return of m.get_items()
