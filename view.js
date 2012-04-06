3dd7feb07f6307190f8a890570431806ff985857
Morgan Paull <mpaull@fas.harvard.edu>
2012-04-05 21:40:33 -0400

    added thumbnails with title and author

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 function View(m){
     //This object draws stuff in HTML
     //View settings go here (i.e. how many things per row, etc)
-	
+	this.gridcolumns = 3;//determines number of thumbnail results displayed per row
 	
 }
 View.prototype.clear_filters = function(){
@@ -38,14 +38,22 @@ View.prototype.draw_filters = function(filters){
 View.prototype.draw_grid = function(item_list){
     //uses return of m.get_items()
     
-    //parse array of results
-    var html = "";
+    var html = '<ul class="thumbnails">';
+    var metadata = '';
     
+    //var html = '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="http://placehold.it/260x180" alt=""><h5>Thumbnail label</h5><p>Thumbnail caption right here...</p></div></li>';
+	//html += '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="http://placehold.it/260x180" alt=""><h5>Thumbnail label</h5><p>Thumbnail caption right here...</p></div></li>';
+	//html += '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="http://placehold.it/260x180" alt=""><h5>Thumbnail label</h5><p>Thumbnail caption right here...</p></div></li>';
+   	//html += '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="http://placehold.it/260x180" alt=""><h5>Thumbnail label</h5><p>Thumbnail caption right here...</p></div></li>';
+   
     
     for (var i = 0; i<item_list.length; i++) {
-    	html += JSON.stringify(item_list[i]) + "<br><br><br>";
+    	html += '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="http://placehold.it/260x180" alt=""><h5>'
+    	html += item_list[i].metadata['title'] + '</h5><p>'
+    	html += item_list[i].metadata['author'] + '</p></div></li>';
+	
     }
-    
+    html += '</ul>'
     $('#results').html(html);
 }
 View.prototype.draw_list = function(item_list){
