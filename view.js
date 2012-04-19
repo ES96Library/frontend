2d57c7812eb73f681c85fec998db436ecb751e93
Morgan Paull <mpaull@fas.harvard.edu>
2012-04-18 21:43:56 -0400

    all results now displayed in individual divs, currently of fixed height, working on making the height intelligent.

--- view.js
+++ view.js
@@ -2,6 +2,7 @@ function View(m){
     //This object draws stuff in HTML
     //View settings go here (i.e. how many things per row, etc)
     this.gridcolumns = 3;//determines size of thumbnails, also determining number of thumbnails displayed per row
+    this.gridheight = 400;//determine the height of each row of the grid view
     this.default_image = 'http://freepages.genealogy.rootsweb.com/~jganis/CabarrusCo/Odell.jpg' //define the default placeholder image
         //'http://www.tullglazener.com/Old%20Rugged%20Cross/Old%20Rugged%20Cross%20-%20harmony.jpg'
 }
@@ -42,11 +43,12 @@ View.prototype.draw_filters = function(filters){
 View.prototype.draw_grid = function(item_list){
     //uses return of m.get_items()
 
-    var html = '<ul class="thumbnails">';
+    var html = '<ul class="thumbnails"><div class="gridrow" style="height:'+this.gridheight+'px">';
     var metadata = '';
+    var counter = 0;
 
     for (var i = 0; i<item_list.length; i++) {
-
+		console.log(counter);
         //check for an image associated with the item, else add the placeholder image
         if(item_list[i].thumb) {
             img = item_list[i].thumb}    		
@@ -54,9 +56,30 @@ View.prototype.draw_grid = function(item_list){
             img = this.default_image;
         };
 
+		
+		
+		//put each row in a grid to keep heights consistant
+		//start a new row only when the previous row is full
+		if(counter >= this.gridcolumns){
+			//close the previous row's div and reset the counter
+			html += '</div>';
+			counter = 0;
+			//open a new row div
+			html += '<div class="gridrow" style="height:'+this.gridheight+'px">';
+		}
 
-        html += '<div data-toggle="modal" item="'+i+'" >'
-            html += '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="' + img + '" alt=""><h6>'+item_list[i].metadata['author']+'</h6>'+item_list[i].metadata['title']+'</div></li></div>'
+		
+
+       		//display the thumbnails
+       		html += '<div data-toggle="modal" item="'+i+'" >';
+      		html += '<li class="span'+ this.gridcolumns + '"><div class="thumbnail"><img src="' + img + '" alt=""><h6>'+item_list[i].metadata['author']+'</h6>'+item_list[i].metadata['title']+'</div></li></div>';
+        	
+        	//increment variable counting thumbnails per row
+        	counter++;
+
+
+
+
 
             /*	
                 html+= '<div class="modal hide fade" id="myModal'+i+'" style="width:700px;margin-left=-350px">'
@@ -79,7 +102,8 @@ View.prototype.draw_grid = function(item_list){
             */
 
     }
-    html += '</ul>'
+    //close the last row div and the unordered list of the thumbnails
+    html += '</div></ul>'
         $('#results').html(html);
 	document.getElementById('loading').style.display = 'none';
 }
